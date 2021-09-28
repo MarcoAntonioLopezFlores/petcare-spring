39712810438bd2255a7115a583f2d6e69ca8e3a8
MarcoLF05 <mnmarcolf@gmail.com>
2021-09-28 14:11:54 -0500

    fist commit

---
+++ src/main/java/mx/app/petcare/PetCareApiSpringApplication.java
@@ -0,0 +1,13 @@
+package mx.app.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/app/petcare/PetCareApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.app.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
