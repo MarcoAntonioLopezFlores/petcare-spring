37166068c05d18ca5e7dd0a1c6143b131e156f72
MarcoLF05 <mnmarcolf@gmail.com>
2021-10-23 16:10:31 -0500

    add: picture controller, dto, service

--- src/main/java/mx/app/petcare/repository/PictureRepository.java
+++ src/main/java/mx/app/petcare/repository/PictureRepository.java
@@ -1,11 +1,16 @@
 package mx.app.petcare.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import mx.app.petcare.entity.Pet;
 import mx.app.petcare.entity.Picture;
+
 
 @Repository
 public interface PictureRepository extends JpaRepository<Picture, Long> {
 
+	public List<Picture> findByPet(Pet pet);
 }
